e540a8be963cda3cbe30d368d65e28e63064e776
magno-brito <magnoleal22@gmail.com>
2023-12-01 10:01:13 -0300

    Creating the first component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherHomeComponent } from './weather/page/weather-home/weather-home/weather-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /* Criando a primeira rota */
+  {
+    path: '',
+    redirectTo: 'weather',
+    pathMatch: 'full'
+  },
+  {
+    path: 'weather',
+    component: WeatherHomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
